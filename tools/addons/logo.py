42820d36e7439a58b618e5efdebd35e01a4297e4
Sanfeire <67929659+Sanfeire@users.noreply.github.com>
2021-06-12 12:09:57 +0200

    Create logo.py

---
+++ tools/addons/logo.py
@@ -0,0 +1,15 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+  _____________________________________________________________________________
+< creado for @breakshacker; para el ilegal de Dani, que a ver que hace con esto >
+ -------------------------------------------------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+{Fore.RESET}""")
